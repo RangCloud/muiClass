909613bdc20b5a24e05a77dbb0f2b5c9729b58e5
RangCloud <mtr008@naver.com>
2023-11-08 13:35:48 +0900

    231108

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import BasicGrid from './components/BasicGrid';
+import BasicAccordion from './components/components_accordion/BasicAccordion';
 
 function App() {
   return (
     <div className="App">
-      <BasicGrid/>
+      <BasicAccordion/>
     </div>
   );
 }
